47710ae7367561d2f22267cb747eaa0b89ad2d1e
Lucas Vinicius <lvininicius@gmail.com>
2023-05-11 19:50:55 -0300

    add mudanças

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 window.onload = function() {
   alert("Seja bem-vindo!");
 };
+
+var isBackgroundVisible = false;
+
+function changeBackground() {
+  var body = document.getElementsByTagName("body")[0];
+  var imagemDiv = document.getElementById("imagem");
+
+  isBackgroundVisible = !isBackgroundVisible;
+
+  body.classList.toggle("light-blue-background");
+  imagemDiv.style.display = isBackgroundVisible ? "block" : "none";
+}
