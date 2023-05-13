c74f8d57ca77d36f04d1f90e190183a6aa4d72c5
Lucas Vinicius <lvininicius@gmail.com>
2023-05-12 22:01:13 -0300

    add form e imagens

--- script.js
+++ script.js
@@ -14,3 +14,25 @@ function changeBackground() {
   body.classList.toggle("light-blue-background");
   imagemDiv.style.display = isBackgroundVisible ? "block" : "none";
 }
+
+ function validar(){
+
+  let q1 = document.querySelector('input[name="q1"]:checked').value;
+  let q2 = document.querySelector('input[name="q2"]:checked').value;
+  let q3 = document.querySelector('input[name="q3"]:checked').value;
+  let score = 0;
+  if (q1 == 'd') {
+    score++;
+  }
+  if (q2 == 'c') {
+    score++;
+  }
+  if (q3 == 'c') {
+    score++;
+  }
+  alert(`Sua pontuação final é ${score} de 3.`);
+}
+
+function redirectToCompra(link) {
+  window.location.href = link;
+}
