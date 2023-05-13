bb208e228ac364fd010d21008d3b1018663f78e1
ZeusBiel <127859799+ZeusBiel@users.noreply.github.com>
2023-05-12 23:44:05 -0300

    Final

---
+++ README.txt
@@ -0,0 +1,7 @@
+Participantes do grupo
+
+1- Gabriel Oliveira Rodrigues RM98565
+2- Gabriel Riqueto RM98685
+3- Lucas Vinicius de Almeida Brigida RM99094
+4- Leandro Ferreira De Morais RM99064
+5- Gustavo Bianchi Velonisqui dos Santos RM98534
--- script.js
+++ script.js
@@ -13,9 +13,10 @@ function changeBackground() {
 
   body.classList.toggle("light-blue-background");
   imagemDiv.style.display = isBackgroundVisible ? "block" : "none";
-}
+};
 
- function validar(){
+
+function validar(){
 
   let q1 = document.querySelector('input[name="q1"]:checked').value;
   let q2 = document.querySelector('input[name="q2"]:checked').value;
@@ -23,16 +24,66 @@ function changeBackground() {
   let score = 0;
   if (q1 == 'd') {
     score++;
-  }
+  };
   if (q2 == 'c') {
-    score++;
-  }
+    score++;;
+  };
   if (q3 == 'c') {
     score++;
-  }
-  alert(`Sua pontuação final é ${score} de 3.`);
-}
+  };
+ alert(`Sua pontuação final é ${score} de 3.`);
+};
 
 function redirectToCompra(link) {
   window.location.href = link;
+};
+
+var imagens = {
+  'fidelito': './imagens/fidelito.jpg',
+  'omanifesto': './imagens/omanifesto.jpg',
+  'marxismo': './imagens/marxismo.jpg',
+  'obra1': './imagens/obra1.jpg',
+  'votos': './imagens/votos.jpg',
+  'arte2': './imagens/arte2.jpg'
+};
+
+var index = 0;
+var time = 4000;
+
+function slideShow(){
+  document.getElementById('slide').src = imagens[Object.keys(imagens)[index]];
+  index++;
+  if (index == Object.keys(imagens).length){index = 0;}
+  setTimeout(slideShow, time);
 }
+
+function enviarFormulario(){
+  let usuario=document.getElementById("usuario").value
+  let email=document.getElementById('email').value
+  let telefone=document.getElementById('telefone').value
+  const validateEmail = (email) => {
+      return email.match(
+       /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+      );
+    };
+  if (email == (email)){
+      alert("mail invalido")    
+  
+  }
+  else{
+      alert("email invalido")
+  }
+  if(usuario =="" && email ==""){
+  alert ('Preencha todos os campos')
+  } 
+  else{
+      alert("Informações enviadas")
+      console.log(usuario, email, telefone)
+  }
+}
+
+slideShow();
+
+
+
+
